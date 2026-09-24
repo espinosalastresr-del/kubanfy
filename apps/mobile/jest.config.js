e9cdce19e86d687559abd1e35e2a70029a5221b3
espinosalastresr-del <espinosalastresr@icloud.com>
2026-09-24 11:52:16 -0400

    test(mobile): map native crypto dependency to Jest mock

--- apps/mobile/jest.config.js
+++ apps/mobile/jest.config.js
@@ -2,6 +2,9 @@ module.exports = {
   preset: 'react-native',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
   testPathIgnorePatterns: ['/node_modules/', '/android/', '/ios/'],
+  moduleNameMapper: {
+    '^react-native-quick-crypto$': '<rootDir>/__mocks__/react-native-quick-crypto.js',
+  },
   transformIgnorePatterns: [
     'node_modules/(?!(react-native|@react-native|@react-navigation)/)',
   ],
